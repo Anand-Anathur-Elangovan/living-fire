971f402383033013e01bf8f3a3ba3ec40dd25e2e
Anand-Anathur-Elangovan <anandanathurelangovan94@gmail.com>
2024-10-16 00:05:55 +0530

    image S3 update
    image S3 update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "living-fire-dev--use1-az4--x-s3.s3express-use1-az4.us-east-1.amazonaws.com",
+    ],
+  },
+};
 
 export default nextConfig;
