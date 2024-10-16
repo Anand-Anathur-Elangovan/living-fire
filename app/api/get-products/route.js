149083cfb826e504a874815e91a91a25788e7409
Naga Sri Muvva <muvva.nagasri@gmail.com>
2024-10-16 20:05:51 +0530

    added 4 apis

---
+++ app/api/get-products/route.js
@@ -0,0 +1,22 @@
+import pool from '../../../utils/db.js';
+
+export async function POST(req) {
+  try {
+    const query = 'SELECT * FROM fn_get_products(0,0,0)';
+    const result = await pool.query(query); // Await the pool query directly
+    return new Response(JSON.stringify(result.rows), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return new Response(JSON.stringify({ error: 'Error fetching users' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
