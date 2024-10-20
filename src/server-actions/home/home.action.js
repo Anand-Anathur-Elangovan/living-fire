58b6d44d7d4b7a7e34903b92f075453c0b054807
Girish <santosh.girish23@gmail.com>
2024-10-20 15:48:01 +0530

    Added Tanstack & React Toastify

--- src/server-actions/home/home.action.js
+++ src/server-actions/home/home.action.js
@@ -9,17 +9,11 @@ import {
 } from "@/src/repo/home/home.repo";
 
 export const getHomePageDataAction = async () => {
-  const collections = [1, 2, 3]; //await getCollectionsRepo().catch((err) => []);
-  const features = ["a", "b", "c"]; //await getFeaturedRepo().catch((err) => []);
-  const brands = ["123d", "asd2", "3213"]; //await getBrandsRepo().catch((err) => []);
-  const userFeedback = ["test", "test"]; //await getUserFeedbackRepo().catch((err) => []);
+  const collections = await getCollectionsRepo().catch((err) => []);
+  const features = await getFeaturedRepo().catch((err) => []);
+  const brands = await getBrandsRepo().catch((err) => []);
+  const userFeedback = await getUserFeedbackRepo().catch((err) => []);
 
-  console.log({
-    ...responsePayload,
-    success: true,
-    message: "Fetched Home data",
-    result: { collections, features, brands, userFeedback },
-  });
   return {
     ...responsePayload,
     success: true,
