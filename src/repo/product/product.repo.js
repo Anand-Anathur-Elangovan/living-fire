a693fee49249f49ac1a2425a23d37d430e033244
Naga Sri Muvva <muvva.nagasri@gmail.com>
2024-10-31 20:57:27 +0530

    added fireplace db function

--- src/repo/product/product.repo.js
+++ src/repo/product/product.repo.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const getProductPage = async () => {
-  const query = "SELECT * FROM fn_get_product_page(1)";
+  const query = "SELECT * FROM fn_get_fireplace_page(1)";
   const result = await prisma
     .$queryRaw(query)
     .then((res) => res)
