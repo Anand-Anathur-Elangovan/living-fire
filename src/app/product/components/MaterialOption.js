e5e67b1cbc9e43ef318610044e482e91164205b0
Anand-Anathur-Elangovan <anandanathurelangovan94@gmail.com>
2024-11-01 10:12:02 +0530

    product page

---
+++ src/app/product/components/MaterialOption.js
@@ -0,0 +1,22 @@
+"use client";
+import "../product.css";
+import Image from 'next/image';
+
+const MaterialOption = ({ options }) => (
+    <div className="col-material">
+      <p className="materialfinish ui text size-h6">Material & Finish Options</p>
+      <div className="colourchoices">
+        {options.map((option, index) => (
+          <div className="colourchoice" key={index}>
+            <Image src={option.imgSrc} alt={option.alt} className="reflectiveblack" layout="responsive" />
+            <label className="choice-radio">
+              <input type="radio" className="ui radio size-xs primary" value={option.value} name="delivery" />
+              <p className="zeroclearancete ui text size-body_medium">{option.description}</p>
+            </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  
+  export default MaterialOption;
