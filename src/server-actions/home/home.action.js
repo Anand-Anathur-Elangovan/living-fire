f50541650a8adfcc5c98afa6552efeaa64dc07ac
Girish <santosh.girish23@gmail.com>
2024-11-17 21:20:16 +0530

    HomePage Repo changes

--- src/server-actions/home/home.action.js
+++ src/server-actions/home/home.action.js
@@ -9,21 +9,22 @@ import {
 } from "@/src/repo/home/home.repo";
 
 export const getHomePageDataAction = async () => {
-  const collections = await getCollectionsRepo().catch((err) => []);
-  const features = await getFeaturedRepo().catch((err) => []);
+  // const collections = await getCollectionsRepo().catch((err) => []);
+  // const features = await getFeaturedRepo().catch((err) => []);
   const brands = await getBrandsRepo()
     .then((res) => res)
     .catch((err) => []);
+  console.log(brands);
   const userFeedback = await getUserFeedbackRepo().catch((err) => []);
   return {
     ...responsePayload,
     success: true,
     message: "Fetched Home data",
     result: {
-      collections,
-      features,
-      brands,
-      userFeedback,
+      collections: [],
+      features: [],
+      brands: brands[0].fn_get_brands,
+      userFeedback: userFeedback.fn_get_user_feedbacks,
     },
   };
 };
