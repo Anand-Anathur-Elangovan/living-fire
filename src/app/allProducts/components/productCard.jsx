d8f05e7f59747d88bf34f2b05fd7da91597cef4d
Girish <santosh.girish23@gmail.com>
2024-11-17 17:39:09 +0530

    All Products Changes

--- src/app/allProducts/components/productCard.jsx
+++ src/app/allProducts/components/productCard.jsx
@@ -9,29 +9,46 @@ import { useRouter } from "next/navigation";
 import { useNavigationState } from "@/context/NavigationContext";
 import { setCookie } from "cookies-next";
 
-const ProductCard = ({ productDetails, addToCompare, isCompare }) => {
+const ProductCard = ({
+  productDetails: { fn_get_products },
+  addToCompare,
+  isCompare,
+}) => {
   const router = useRouter();
   const { setNavigationState } = useNavigationState();
   // console.log(productDetails);
   const imageURL = useMemo(() => {
-    if (productDetails.fn_get_products.hero_image) {
-      let heroimage;
-      try {
-        heroimage = JSON.parse(
-          productDetails.fn_get_products.hero_image?.replace(/'/g, '"')
-        );
-      } catch {
-        heroimage = null;
-      }
-      return heroimage && heroimage.length > 0
-        ? heroimage[0].value?.includes("http")
-          ? heroimage[0]?.value
-          : null
-        : null;
+    if (fn_get_products.hero_image) {
+      if (fn_get_products.hero_image[0].value.includes("http"))
+        return fn_get_products.hero_image[0].value;
     }
     return null;
   });
-  // console.log(imageURL, "imageURL");
+
+  const brochureURL = useMemo(() => {
+    if (fn_get_products.brochure) {
+      if (fn_get_products.brochure[0].value.includes("http"))
+        return fn_get_products.brochure[0].value;
+    }
+    return null;
+  });
+
+  const pricebookURL = useMemo(() => {
+    if (fn_get_products.pricebook) {
+      if (fn_get_products.pricebook[0].value.includes("http"))
+        return fn_get_products.pricebook[0].value;
+    }
+    return null;
+  });
+
+  const specSheetURL = useMemo(() => {
+    if (fn_get_products.spec_sheet) {
+      if (fn_get_products.spec_sheet[0].value.includes("http"))
+        return fn_get_products.spec_sheet[0].value;
+    }
+    return null;
+  });
+
   const handleProductClick = (productId) => {
     setNavigationState({ productId });
     setCookie(
@@ -58,41 +75,72 @@ const ProductCard = ({ productDetails, addToCompare, isCompare }) => {
               id="checkbox"
               className="h-5 w-5 rounded-lg appearance-auto checked:bg-black"
               onChange={(e) =>
-                addToCompare(
-                  productDetails.fn_get_products.p_id,
-                  e.target.checked
-                )
+                addToCompare(fn_get_products.p_id, e.target.checked)
               }
             />
           </div>
         )}
         <Image
           src={imageURL ? imageURL : CheckerBoardImg}
-          alt={productDetails.fn_get_products.p_name} //productDetails.fn_get_products.p_name
+          alt={fn_get_products.p_name ?? ""} //productDetails.fn_get_products.p_name
           className="element-image"
           width={300} // specify your desired width
           height={600} // specify your desired height
-          onClick={() =>
-            handleProductClick(productDetails?.fn_get_products?.p_id)
-          }
+          onClick={() => handleProductClick(fn_get_products?.p_id)}
         />
       </div>
       <div className="py-2 gap-3 ">
         <h3 className="font-sans font-medium leading-6 text-base text-wrap">
-          {productDetails.fn_get_products.p_name}
+          {fn_get_products.p_name}
         </h3>
         <div className="flex flex-row justify-between mr-5">
           <span className="font-sans font-normal leading-5 text-sm">
-            {productDetails?.fn_get_products?.brand_name}
+            {fn_get_products?.brand_name}
           </span>
           <div className="flex gap-1">
             {false ? (
-              <>{productDetails?.fn_get_products?.price}</>
+              <>{fn_get_products?.price}</>
             ) : (
               <>
-                <Image src={NoPriceIcon} alt="no Price" />
-                <Image src={NoPriceIcon} alt="no Price" />
-                <Image src={NoPriceIcon} alt="no Price" />
+                {brochureURL && (
+                  <a
+                    href={brochureURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="tooltip"
+                  >
+                    <span className="tooltiptext font-sans font-light leading-6 text-base text-wrap text-xs">
+                      Download brochure
+                    </span>
+                    <Image src={NoPriceIcon} alt="Brochure" />
+                  </a>
+                )}
+                {pricebookURL && (
+                  <a
+                    href={pricebookURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="tooltip"
+                  >
+                    <span className="tooltiptext font-sans font-light leading-6 text-base text-wrap text-xs">
+                      Download Price Book
+                    </span>
+                    <Image src={NoPriceIcon} alt="Price Book" />
+                  </a>
+                )}
+                {specSheetURL && (
+                  <a
+                    href={specSheetURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="tooltip"
+                  >
+                    <span className="tooltiptext font-sans font-light leading-6 text-base text-wrap text-xs">
+                      Download Spec Sheet
+                    </span>
+                    <Image src={NoPriceIcon} alt="Spec Sheet" />
+                  </a>
+                )}
               </>
             )}
           </div>
@@ -103,3 +151,17 @@ const ProductCard = ({ productDetails, addToCompare, isCompare }) => {
 };
 
 export default ProductCard;
+
+//// let heroimage;
+// try {
+//   heroimage = JSON.parse(
+//     productDetails.fn_get_products.hero_image?.replace(/'/g, '"')
+//   );
+// } catch {
+//   heroimage = null;
+// }
+// return heroimage && heroimage.length > 0
+//   ? heroimage[0].value?.includes("http")
+//     ? heroimage[0]?.value
+//     : null
+//   : null;
--- src/app/allProducts/hooks/useAllProducts.js
+++ src/app/allProducts/hooks/useAllProducts.js
@@ -1,23 +1,55 @@
-import { getAllProductsPageDataAction } from "@/src/server-actions/all-products/all-products.action";
+import {
+  getAllProductsPageDataAction,
+  getMasterValuesAction,
+} from "@/src/server-actions/all-products/all-products.action";
 import { useQuery } from "@tanstack/react-query";
 
 import { toast } from "react-toastify";
 import useHomePage from "../../home/hooks/useHomePage";
+import { useState } from "react";
 
-const useAllProducts = (type_id) => {
+const useAllProducts = (
+  type_id,
+  fireplaceType,
+  brandType,
+  bestSelling,
+  searchText,
+  subType
+) => {
   const initialData = [];
   const { data: allProducts = initialData, isFetched } = useQuery({
-    queryKey: ["getAllProductsAction" + type_id],
-    queryFn: () => getAllProductsPageDataAction(type_id),
+    queryKey: [
+      "getAllProductsAction",
+      type_id,
+      fireplaceType,
+      brandType,
+      bestSelling,
+      searchText,
+      subType,
+    ],
+    queryFn: () =>
+      getAllProductsPageDataAction({
+        type_id,
+        fireplaceType,
+        brandType,
+        bestSelling,
+        searchText,
+        subType,
+      }),
+    staleTime: 0,
+    // enabled: productMenuIndex ? true : false,
     select: (res) => {
-      // console.log(res);
+      console.log(res.result, "HOOCK");
       if (res.success) return res.result;
       toast.error(res.message);
       return initialData;
     },
   });
 
-  return { allProducts, isFetched };
+  return {
+    allProducts,
+    isFetched,
+  };
 };
 
 export default useAllProducts;
