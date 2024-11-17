9cb5a385ca60bf1902fca7d9b0697c06be3b5785
Anand-Anathur-Elangovan <anandanathurelangovan94@gmail.com>
2024-11-17 23:37:39 +1100

    check

--- src/repo/product/product.repo.js
+++ src/repo/product/product.repo.js
@@ -6,10 +6,10 @@ const prisma = new PrismaClient();
 export const getProductPage = async (productId) => {
   const cookieStore = cookies();
   const cookieProductId = cookieStore.get("selectedProductId")?.value;
-  // const query = `select * from fn_get_product_page(${
-  //   productId ? productId : cookieProductId ? cookieProductId : "424"
-  // })`;
-  const query = "select * FROM fn_get_product_page(424)";
+  const query = `select * from fn_get_product_page(${
+    productId ? productId : cookieProductId ? cookieProductId : "424"
+  })`;
+  // const query = "select * FROM fn_get_product_page(424)";
   try {
     const result = await prisma.$queryRawUnsafe(query);
     return result;
