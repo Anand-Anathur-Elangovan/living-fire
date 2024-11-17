51e700cdcec6c394bd44f83971555d85c3380e69
Anand-Anathur-Elangovan <anandanathurelangovan94@gmail.com>
2024-11-18 01:37:55 +1100

    check

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
       },
     ];
   },
-
+  serverComponentsExternalPackages: ["prisma", "pg"],
   // Enabling experimental features for server actions
   experimental: {
     serverActions: true,
