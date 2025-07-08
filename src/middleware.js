d83e4e3e016a26410a8a24cffd964863974c782b
Anand-Anathur-Elangovan <anandanathurelangovan94@gmail.com>
2025-07-08 23:30:47 +0530

    c

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-export const runtime = 'edge'; 
+// export const runtime = 'edge'; 
   // Define URL alias mappings
   const urlAliases = {
   "/discount-fireplaces/albany-wood-heater": "/regency/albany",
