607fdef3a49ba393f11321ba01fd6e9ca61a9ac2
Anand-Anathur-Elangovan <anandanathurelangovan94@gmail.com>
2025-07-21 01:33:20 +0530

    check

---
+++ src/components/metaData/bosqProductsMetaData.js
@@ -0,0 +1,29 @@
+export const bosqProductsMetaData = [
+  {
+    brand: "Bosq",
+    name: "Aere 70S Insert",
+    slug: "aere-70s-insert",
+    title: "Bosq Aere 70S Insert - Living Fire",
+    description: "",
+    type: "product",
+    keywords: "Bosq, Aere 70S Insert, freestanding fireplace, wood fireplace, sustainable fireplace, eco-friendly fireplace, Living Fire"
+  },
+  {
+    brand: "Bosq",
+    name: "Aere 70S Freestanding",
+    slug: "aere-70s-freestanding",
+    title: "Bosq Aere 70S Freestanding Fireplace - Living Fire",
+    description: "",
+    type: "product",
+    keywords: "Bosq, Aere 70S Freestanding, freestanding fireplace, wood fireplace, luxury fireplace, Living Fire"
+  },
+  {
+    brand: "Bosq",
+    name: "Aere 70S Freestanding L - Black Steel Base",
+    slug: "aere-70s-freestanding-l-black-steel-base",
+    title: "Bosq Aere 70S Large Freestanding Fireplace - Living Fire",
+    description: "",
+    type: "product",
+    keywords: "Bosq, Aere 70S Freestanding L - Black Steel Base, freestanding fireplace, wood fireplace, luxury fireplace, Living Fire"
+  }
+]
